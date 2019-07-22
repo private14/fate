aeac3cbe5f6656f040e5e8e5508b1e00ac5f35b2
wanyl <zcdsw@sina.com>
2019-07-22 11:49:04 +0800

    11

--- common_function/operate_sql.py
+++ common_function/operate_sql.py
@@ -21,6 +21,10 @@ def select_mysql(sql, mysql_dict):
     cursor.execute(sql)
     data = cursor.fetchall()
     assert data is not None
+    if len(list(data)) == 1:
+        data = list(list(data)[0])
+    else:
+        data = list(data)
     cursor.close()
     db.close()
     return data
@@ -29,25 +33,6 @@ def select_mysql(sql, mysql_dict):
 # 操作
 def operate_mysql(sql, mysql_dict):
     db = connect_mysql(mysql_dict)
-    cursor = db.cursor()
-    cursor.execute(sql)
-    db.commit()
-    cursor.close()
-    db.close()
-
-
-# 复制数据
-def copy_data(sql, mysql_dict, copy_to_mysql_dict):
-    # 经销商数据迁移
-    resource_credit_merchant_sign_info = select_mysql(sql=sql, mysql_dict=mysql_dict)
-    print(select_mysql(
-        'select column_name from information_schema.columns where table_name = "credit_merchant_sign_info";',
-        sit1_connect_dict_all['credittrans']))
-    # operate_mysql('INSERT INTO credit_merchant_sign_info')
-
-
-
-db = connect_mysql(mysql_dict)
     cursor = db.cursor()
     cursor.execute(sql)
     db.commit()
--- data_migration/views.py
+++ data_migration/views.py
@@ -5,6 +5,7 @@ from accounts.models import *
 from django.forms.models import model_to_dict
 from common_function.operate_sql import *
 import json
+import datetime
 
 
 class DataView(View):
@@ -24,20 +25,22 @@ class DataView(View):
 
     # 从sit1 copy
     @staticmethod
-    def copy_merchant_id_by_sit1(env_name, id):
-        sit1_dict = DataView.get_modules('sit1')
+    def copy_merchant_id_by_sit1(env_name, id, source='sit1'):
+        sit1_dict = DataView.get_modules(source)
         env_dict = DataView.get_modules(env_name)
-        # 查询需要被copy数据
+        # 查询需要被copy sql 链接信息
         sit1_connect_dict_all = eval(sit1_dict['sqlDict'])
+        env_connect_dict_all = eval(env_dict['sqlDict'])
         # 经销商数据迁移
-        sit1_credit_merchant_sign_info = select_mysql('select * from credit_merchant_sign_info where Merchant_ID= "' + id + '";', sit1_connect_dict_all['credittrans'])
-        #print(sit1_credit_merchant_sign_info)
-        #print(select_mysql('show columns from credit_merchant_sign_info;', sit1_connect_dict_all['credittrans']))
-        print(select_mysql('select column_name from information_schema.columns where table_name = "credit_merchant_sign_info";', sit1_connect_dict_all['credittrans']))
-        #operate_mysql('INSERT INTO credit_merchant_sign_info')
+        sit1_credit_merchant_sign_info = select_mysql(
+            'select * from credit_merchant_sign_info where Merchant_ID= "' + id + '";',
+            sit1_connect_dict_all['credittrans'])
+        filter = []
+        for i in sit1_credit_merchant_sign_info:
+            if type(i) is datetime.datetime or type(i) is datetime.date:
+                filter.append(str(i))
+            else:
+                filter.append(i)
 
-
-
-
-
-
+        print('INSERT INTO credit_merchant_sign_info VALUES ' + str(tuple(filter)) + ';')
+        operate_mysql('INSERT INTO credit_merchant_sign_info VALUES' + str(tuple(filter)) + ';', env_connect_dict_all['credittrans'])
